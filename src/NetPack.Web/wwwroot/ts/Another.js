a9a04220ebbb09d59854f88b5c63f4381e7fc06c
dazinator <darrell.tunnell@googlemail.com>
2018-09-12 20:51:35 +0100

    closes #21 Upgraded solution to vs2017

---
+++ src/NetPack.Web/wwwroot/ts/Another.js
@@ -0,0 +1,18 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Another = /** @class */ (function () {
+        function Another(another) {
+            this.another = another;
+        }
+        Another.prototype.greet = function () {
+            return " < h1 > " + this.another + " < /h1>";
+        };
+        return Another;
+    }());
+    exports.Another = Another;
+    ;
+    var another = new Another("seasons greetings!");
+    document.body.querySelector(".message").innerHTML += another.greet();
+});
+//# sourceMappingURL=Another.js.map
