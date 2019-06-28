6f7045e60000ca0ae98f6c31ae4442d7f2c9c4a5
Darrell Tunnell <darrell.tunnell@covarius.com>
2019-06-28 15:50:18 +0100

    Added blazor helper method

--- src/NetPack.Blazor/BlazorClientAppFileProviderHelper.cs
+++ src/NetPack.Blazor/BlazorClientAppFileProviderHelper.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.Extensions.FileProviders;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Net.Http.Headers;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Mime;
 
 namespace NetPack.Blazor
 {
     public static class BlazorClientAppFileProviderHelper
     {
-       
         public static IFileProvider GetFileProvider<TClientApp>()
         {
             var assemblylocation = typeof(TClientApp).Assembly.Location;
@@ -30,7 +35,7 @@ namespace NetPack.Blazor
             bool distExists = Directory.Exists(distPath);
 
             var fileProviders = new List<IFileProvider>();
-            if(webRootPathExists)
+            if (webRootPathExists)
             {
                 fileProviders.Add(new PhysicalFileProvider(webRootPath));
             }
@@ -40,13 +45,72 @@ namespace NetPack.Blazor
                 fileProviders.Add(new PhysicalFileProvider(distPath));
             }
 
-            if(fileProviders.Count > 1)
+            if (fileProviders.Count > 1)
             {
                 return new CompositeFileProvider(fileProviders);
             }
             else
             {
                 return fileProviders.FirstOrDefault();
+            }
+        }
+
+        /// <summary>
+        /// Adds a <see cref="StaticFileMiddleware"/> that will serve static files from the client-side Blazor application
+        /// specified by <paramref name="clientAssemblyFilePath"/>.
+        /// </summary>
+        /// <param name="clientAssemblyFilePath">The file path of the client-side Blazor application assembly.</param>
+        /// <param name="app">The <see cref="IApplicationBuilder"/>.</param>
+        /// <returns>The <see cref="IApplicationBuilder"/>.</returns>
+        public static IApplicationBuilder UseClientSideBlazorFiles(this IApplicationBuilder app, IFileProvider fileProvider, bool enableDebugging)
+        {
+            if (fileProvider == null)
+            {
+                throw new ArgumentNullException(nameof(fileProvider));
+            }
+
+            var contentTypeProvider = new FileExtensionContentTypeProvider();
+            AddMapping(contentTypeProvider, ".dll", MediaTypeNames.Application.Octet);
+            if (enableDebugging)
+            {
+                AddMapping(contentTypeProvider, ".pdb", MediaTypeNames.Application.Octet);
+            }
+
+            var options = new StaticFileOptions()
+            {
+                ContentTypeProvider = contentTypeProvider,
+                FileProvider = fileProvider,
+                OnPrepareResponse = SetCacheHeaders,
+            };
+
+            app.UseStaticFiles(options);
+            return app;
+
+
+        }
+        static void AddMapping(FileExtensionContentTypeProvider provider, string name, string mimeType)
+        {
+            if (!provider.Mappings.ContainsKey(name))
+            {
+                provider.Mappings.Add(name, mimeType);
+            }
+        }
+
+        internal static void SetCacheHeaders(StaticFileResponseContext ctx)
+        {
+            // By setting "Cache-Control: no-cache", we're allowing the browser to store
+            // a cached copy of the response, but telling it that it must check with the
+            // server for modifications (based on Etag) before using that cached copy.
+            // Longer term, we should generate URLs based on content hashes (at least
+            // for published apps) so that the browser doesn't need to make any requests
+            // for unchanged files.
+            var headers = ctx.Context.Response.GetTypedHeaders();
+            if (headers.CacheControl == null)
+            {
+                headers.CacheControl = new CacheControlHeaderValue
+                {
+                    NoCache = true
+                };
             }
         }
     }
